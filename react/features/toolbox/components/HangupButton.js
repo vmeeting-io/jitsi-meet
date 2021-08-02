add7e26e1c5d3001d1e3576c959e50884a8189c6
TaeHwan Eun <theun@postech.ac.kr>
2021-08-02 08:55:12 +0000

    fix: Issue #46

--- react/features/toolbox/components/HangupButton.js
+++ react/features/toolbox/components/HangupButton.js
@@ -147,6 +147,7 @@ class HangupButton extends AbstractHangupButton<Props, *> {
 
     _renderModeratorSelectionContent() {
         const { _participants, _selected, t } = this.props;
+        const selected = this.state.selected || _selected;
 
         if (_participants.length <= 1)
             return [];
@@ -157,7 +158,7 @@ class HangupButton extends AbstractHangupButton<Props, *> {
                     key: item.id,
                     accessibilityLabel: t('toolbar.accessibilityLabel.moderatorSelectionList'),
                     text: item.name,
-                    selected: item.id === _selected,
+                    selected: item.id === selected,
                     ...item
                 })) }
             </ul>,

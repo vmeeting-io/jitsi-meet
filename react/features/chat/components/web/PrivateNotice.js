d80ab18684f86a73f2bc4ef998c2f16b489ef192
sajan-dpnm <thesajan@postech.ac.kr>
2021-08-18 10:57:06 +0900

    Created new file for private notice

---
+++ react/features/chat/components/web/PrivateNotice.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { MESSAGE_TYPE_LOCAL } from '../../constants';
+declare var APP: Object;
+
+/**
+ * The type of the React {@code Component} state of {@code PrivateNotice}.
+ */
+
+/**
+ * The type of the React {@code Component} props of {@code PrivateNotice}.
+ */
+type Props = {
+    /**
+     * The representation of a chat message.
+     */
+    message: Object,
+
+    /**
+     * Name of the participant entitled to receive a private message
+     */
+    privateMessageRecipient: String,
+};
+
+export default class PrivateNotice extends Component<Props, State> {
+    constructor(props) {
+        super(props);
+
+        // Bind event handler so it is only bound once for every instance.
+        this._getPrivateNoticeMessage = this._getPrivateNoticeMessage.bind(this);
+    }
+
+    render() {
+        return(
+            <div className = 'privatemessagenotice'>
+                { this._getPrivateNoticeMessage() }
+            </div>
+        )
+    }
+
+    _getPrivateNoticeMessage: () => String;
+
+    _getPrivateNoticeMessage = () => {
+        const { message, t } = this.props;
+        console.log("Inside _getPrivateNoticeMessage");
+        console.log("this props are: ", this.props);
+        
+        return t('chat.privateNotice', {
+            recipient: message.messageType === MESSAGE_TYPE_LOCAL ? privateMessageRecipient : t('me')
+        });
+    }
+
+    
+}
+
+
+
+

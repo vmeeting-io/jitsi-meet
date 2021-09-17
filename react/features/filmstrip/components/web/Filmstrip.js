c34fc58f966f98bff98fc967b7dc5e516c6422ae
sajan-dpnm <thesajan@postech.ac.kr>
2021-09-17 11:53:47 +0900

    Added CSS to display an overlay of random selection countdown

--- react/features/filmstrip/components/web/Filmstrip.js
+++ react/features/filmstrip/components/web/Filmstrip.js
@@ -199,6 +199,10 @@ class Filmstrip extends PureComponent <Props> {
             <div
                 className = { `filmstrip ${this.props._className}` }
                 style = { filmstripStyle }>
+                    <div className = 'filmstrip__countdown' id='filmstrip__countdown'>
+                        {/* should call the function that automatically blinks the text and runs a timer */}
+                        { 5 }
+                    </div>
                 { toolbar }
                 <div
                     className = { this.props._videosClassName }

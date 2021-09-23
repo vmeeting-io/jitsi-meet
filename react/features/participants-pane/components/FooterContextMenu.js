bb3b2991a9a776eb54d54ef6349147d39aa9f8c7
sajan-dpnm <thesajan@postech.ac.kr>
2021-09-23 21:57:13 +0900

    Disable random selection menu in participant pane if random selection is currently running

--- react/features/participants-pane/components/FooterContextMenu.js
+++ react/features/participants-pane/components/FooterContextMenu.js
@@ -42,6 +42,15 @@ const useStyles = makeStyles(() => {
             transform: 'translateY(-100%)',
             width: '238px'
         },
+        menudisabled: {
+            pointerEvents: 'none',
+            cursor: 'not-allowed',
+            opacity: 0.65,
+            filter: 'alpha(opacity=65)',
+            WebkitBoxShadow: 'none',
+            BoxShadow: 'none',
+        },
+
         text: {
             marginLeft: '52px',
             lineHeight: '40px'
@@ -77,6 +86,8 @@ export const FooterContextMenu = ({ onMouseLeave }: Props) => {
     const enable = useCallback(() => dispatch(requestEnableModeration()), [ dispatch ]);
 
     const classes = useStyles();
+    let isRandomSelectionRunning = (APP.store.getState()['features/base/conference'].startCountdown === true) ? true : false;
+    const randomselectionClass = isRandomSelectionRunning ? classes.menudisabled : '';
 
     const muteAllVideo = useCallback(
         () => dispatch(openDialog(MuteEveryonesVideoDialog, { exclude: [ id ] })), [ dispatch ]);
@@ -110,6 +121,7 @@ export const FooterContextMenu = ({ onMouseLeave }: Props) => {
 
             {/* context menu item for random selection */}
             <ContextMenuItem
+                className = { randomselectionClass }
                 id = 'participants-pane-context-menu-random-selection'
                 onClick = { startRandomSelection }>
                 <Icon

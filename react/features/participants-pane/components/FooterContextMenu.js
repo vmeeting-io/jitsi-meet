6ee4cbdbfa8c26512f9ee57bf6f47104c3d6b717
sajan-dpnm <thesajan@postech.ac.kr>
2021-09-24 16:55:04 +0900

    Change random selection to proceed with minimum 3 participants and moderator is not included during selection

--- react/features/participants-pane/components/FooterContextMenu.js
+++ react/features/participants-pane/components/FooterContextMenu.js
@@ -15,6 +15,7 @@ import { Icon, IconCheck, IconVideoOff, IconAnnouncement, IconStopWatch } from '
 import { MEDIA_TYPE } from '../../base/media';
 import {
     getLocalParticipant,
+    getParticipantCount,
     getParticipantDisplayName,
     isEveryoneModerator
 } from '../../base/participants';
@@ -89,6 +90,8 @@ export const FooterContextMenu = ({ onMouseLeave }: Props) => {
     let isRandomSelectionRunning = (APP.store.getState()['features/base/conference'].startCountdown === true) ? true : false;
     const randomselectionClass = isRandomSelectionRunning ? classes.menudisabled : '';
 
+    const participantCount = getParticipantCount(APP.store.getState());
+
     const muteAllVideo = useCallback(
         () => dispatch(openDialog(MuteEveryonesVideoDialog, { exclude: [ id ] })), [ dispatch ]);
 
@@ -124,15 +127,19 @@ export const FooterContextMenu = ({ onMouseLeave }: Props) => {
             onMouseLeave = { onMouseLeave }>
 
             {/* context menu item for random selection */}
-            <ContextMenuItem
-                className = { randomselectionClass }
-                id = 'participants-pane-context-menu-random-selection'
-                onClick = { startRandomSelection }>
-                <Icon
-                    size = { 18 }
-                    src = { IconAnnouncement } />
-                <span>{ t('participantsPane.actions.startRandomSelection') }</span>
-            </ContextMenuItem>
+            { 
+                participantCount >= 3 
+                ? <ContextMenuItem
+                        className = { randomselectionClass }
+                        id = 'participants-pane-context-menu-random-selection'
+                        onClick = { startRandomSelection }>
+                        <Icon
+                            size = { 18 }
+                            src = { IconAnnouncement } />
+                        <span>{ t('participantsPane.actions.startRandomSelection') }</span>
+                </ContextMenuItem>
+                : <></>
+            }
 
             {/* context menu item for timer function */}
             <ContextMenuItem

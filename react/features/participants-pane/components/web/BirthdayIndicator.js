b4e6eb79962deae811accbb84ab522d8f7eea7c0
Anish Byanjankar <anish@kedutech.kr>
2021-10-05 10:14:40 +0900

    Changed Birthday Cake icon.

--- react/features/participants-pane/components/web/BirthdayIndicator.js
+++ react/features/participants-pane/components/web/BirthdayIndicator.js
@@ -7,9 +7,7 @@ import { Icon, IconBirthdayCake } from '../../../base/icons';
 import { BirthdayIndicatorBackground } from './styled';
 
 export const BirthdayIndicator = () => (
-    <BirthdayIndicatorBackground>
         <Icon
             size = { 15 }
             src = { IconBirthdayCake } />
-    </BirthdayIndicatorBackground>
 );

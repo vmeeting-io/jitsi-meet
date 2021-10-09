7095ddb758fb36a28a2f5ce1213cac3648a38234
Anish Byanjankar <anish@kedutech.kr>
2021-10-09 19:19:05 +0900

    Send endtime and initiator of timer to xmpp.

--- react/features/participants-pane/actions.any.js
+++ react/features/participants-pane/actions.any.js
@@ -11,6 +11,9 @@ import {
     getRemoteParticipants
 } from '../base/participants';
 
+import { COMMAND_TIMER_END_TIME
+} from './constants';
+
 /**
  * Action to close the participants pane.
  *
@@ -49,6 +52,13 @@ export const notifyRandomSelectionStarted = (initiator) => {
     const state = APP.store.getState();
     const { conference } = state['features/base/conference'];
     conference.startTimer(initiator,endUNIXTime);
+    
+    // Send message to XMPP module
+    conference.sendMessage({
+        type: COMMAND_TIMER_END_TIME,
+        timerEndTime: endUNIXTime,
+        senderName: initiator
+    });
 };
 
 /**

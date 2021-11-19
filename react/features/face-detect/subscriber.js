fd79d81715773f270ddb4dea420f38a1651706f0
TaeHwan Eun <theun0524@gmail.com>
2021-11-19 06:31:53 +0000

    fix: update latest lib-jitsi-meet fix: add exception handle

--- react/features/face-detect/subscriber.js
+++ react/features/face-detect/subscriber.js
@@ -63,6 +63,8 @@ StateListenerRegistry.register(
             }
         };
 
-        conference.on(JitsiConferenceEvents.NON_PARTICIPANT_MESSAGE_RECEIVED, receiveMessage);
+        if (conference) {
+            conference.on(JitsiConferenceEvents.NON_PARTICIPANT_MESSAGE_RECEIVED, receiveMessage);
+        }
     }
 )

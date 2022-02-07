c51ef3f9af22d6443c006bbe3ad308b229155daf
TaeHwan Eun <taehwan.eun@kedutech.kr>
2022-02-07 04:59:46 +0000

    fix: update language url

--- react/features/base/i18n/i18next.js
+++ react/features/base/i18n/i18next.js
@@ -40,7 +40,7 @@ export const DEFAULT_LANGUAGE = LANGUAGES[0];
  */
 const options = {
     backend: {
-        loadPath: 'lang/{{ns}}-{{lng}}.json?v=1'
+        loadPath: 'lang/{{ns}}-{{lng}}.json?v=2'
     },
     defaultNS: 'main',
     fallbackLng: DEFAULT_LANGUAGE,

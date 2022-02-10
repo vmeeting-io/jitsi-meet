941a4a5af4fd449b008773a551062ebf4dff0629
TaeHwan Eun <taehwan.eun@kedutech.kr>
2022-02-10 16:54:06 +0900

    fix: remove env-config.js

--- config.js
+++ config.js
@@ -1192,18 +1192,23 @@ var config = {
 
     // thirdPartyAuth: {
     //     postech: {
+    //         label: false,
     //         url: 'https://login.postech.ac.kr/SSOService.do?targetAppId=postech-vmeetingKr-web',
     //     },
     //     google:{
+    //         label: false,
     //         url: '/auth/api/complete/google/login',
     //     },
     //     facebook:{
+    //         label: false,
     //         url: '/auth/api/complete/facebook/login',
     //     },
     //     naver:{
+    //         label: false,
     //         url: '/auth/api/complete/naver/login',
     //     },
     //     kakao:{
+    //         label: false,
     //         url: '/auth/api/complete/kakao/login',
     //     },
     // },

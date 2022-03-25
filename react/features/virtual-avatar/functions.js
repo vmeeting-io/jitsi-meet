5b4460d1c1daa823893c92a32cba1df1b30b70c5
TaeHwan Eun <taehwan.eun@kedutech.kr>
2022-03-25 17:09:23 +0900

    fix: typo fix

--- react/features/virtual-avatar/functions.js
+++ react/features/virtual-avatar/functions.js
@@ -4,6 +4,27 @@ import { JitsiTrackEvents } from '../base/lib-jitsi-meet';
 import { updateSettings } from '../base/settings';
 
 import { toggleVirtualAvatarEffect } from './actions';
+let filterSupport;
+
+declare var APP: Object;
+
+/**
+ * Checks context filter support.
+ *
+ * @returns {boolean} True if the filter is supported and false if the filter is not supported by the browser.
+ */
+export function checkBlurSupport() {
+    if (typeof filterSupport === 'undefined') {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        filterSupport = typeof ctx.filter !== 'undefined';
+
+        canvas.remove();
+    }
+
+    return filterSupport;
+}
 
 /**
  * Convert blob to base64.
